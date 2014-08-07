d074fa99ebb1f982eb3323cb6cd87b56135a48d7
samandlorien <spam@yahoo.com>
2014-08-07 19:08:58 -0400

    changed nav menu
    no longer using a pre html5 hack.  we are now fully committed to using
    the html5 nav tag.  also moved the nav menu to a javascript file so its
    not duplicated.

---
+++ githubwebsite/githubwebsite/nav_menu.js
@@ -0,0 +1,16 @@
+﻿// taken from http://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file/15250208#15250208
+var pages = [ ["food.html", "Meal Plan"], ["calendar.html", "Calendar"], ["events.html", "Activities"] ];
+
+document.write('<div class="nav_div">\
+                    <nav>');
+for(var i = 0; i < pages.length; ++i)
+{
+    document.write('<a ');
+    if (pages[i][0] === location.pathname.substring(1))
+    {
+        document.write('class="selected" ');
+    }
+    document.write('href="' + pages[i][0] + '">' + pages[i][1] + '</a>');
+}
+document.write('    </nav>\
+                </div>');

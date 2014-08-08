9ded654a2add4cd4e8f47a720f018587aeddd0c3
samandlorien <spam@yahoo.com>
2014-08-08 18:29:33 -0400

    updated upload
    updated upload to now force the reaction to be an integer

--- githubwebsite/githubwebsite/upload.php
+++ githubwebsite/githubwebsite/upload.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_POST['food_allergy'])){
    $json_food = $_POST['food_allergy'];
+   $json_food['reaction']=(int)$json_food['reaction'];  // because it comes over as a string
    $raw_file = file_get_contents('food.json');
    $json_file = json_decode($raw_file, true);
    array_push( $json_file['allergies'], $json_food );

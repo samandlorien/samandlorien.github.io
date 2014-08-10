6d82f6ac15d5c0d56ae7e7f519c68a979108a0dc
samandlorien <spam@yahoo.com>
2014-08-09 23:51:11 -0400

    food assignment
    added a mechanism to sign up for various food times.

--- githubwebsite/githubwebsite/upload.php
+++ githubwebsite/githubwebsite/upload.php
@@ -12,62 +12,15 @@ if(isset($_POST['new_chef']))
    $json_chef = $_POST['new_chef'];
    $raw_file = file_get_contents('food.json');
    $json_file = json_decode($raw_file, true);
-   
-   
-   echo json_encode($json_file['calendar']);
-  // foreach($json_file['calendar'] as $day)
-for($i=0; $i<count($json_file['calendar']); $i++) 
+   for($i=0; $i<count($json_file['calendar']); $i++) 
    {
-   $day = $json_file['calendar'][$i];
-		if($day['day'] == (int)$json_chef['day'])
-		{
-		echo "day ";
-		echo $day['day'];
-		echo (int)$json_chef['day'];
-		switch($json_chef['time'])
-	   {
-	   case '1':
-	   echo "chef1";
-	   var_export($day['chef1']);
-	   echo "------------";
-	   echo $json_chef['chef'];
-	   $day['chef1'] = $json_chef['chef'];
-	   echo $day['chef1'];
-	   //$json_file['calendar'][i].chef1 = (int)$json_chef['chef'];
-	   echo "sadfasdf---------------|";
-	   echo $json_file['calendar'][$i]['chef1'];
-	   echo "|";
-	   $json_file['calendar'][$i]['chef1'] = $json_chef['chef'];
-	   echo $json_file['calendar'][$i]['chef1'];
-	   break;
-	   case '2':
-	   //$json_file['calendar'][i].chef2 = $json_chef['chef'];
-	   break;
-	   case '3':
-	   //$json_file['calendar'][i].chef3 = $json_chef['chef'];
-	   break;
-	   }
-		}
+      if($json_file['calendar'][$i]['day'] == (int)$json_chef['day'])
+	  {
+         $chef_string = 'chef' . $json_chef['time'];
+         $json_file['calendar'][$i][$chef_string] = $json_chef['chef'];
+      }
    }
-   /*for(int i = 0; i < $json_file['calendar']->length; ++i)
-   {
-	if ($json_file['calendar'][i].day == (int)$json_chef['day'])
-	{
-	   switch((int)$json_chef['time'])
-	   {
-	   case 1:
-	   $json_file['calendar'][i].chef1 = $json_chef['chef'];
-	   break;
-	   case 2:
-	   $json_file['calendar'][i].chef2 = $json_chef['chef'];
-	   break;
-	   case 3:
-	   $json_file['calendar'][i].chef3 = $json_chef['chef'];
-	   break;
-	   }
-    }
-   }*/
-    echo json_encode($json_file['calendar']);
+   file_put_contents('food.json', json_encode($json_file));
 }
 // other uploads?
 ?>
